78f314605b1b5498bbd4386d07f4f369d191013c
Geekbee <39613357+GeekbeeGER@users.noreply.github.com>
2025-06-11 00:17:21 +0200

    Update entrypoint.sh

--- cod2/bin/entrypoint.sh
+++ cod2/bin/entrypoint.sh
@@ -2,19 +2,23 @@
 set -e
 cd /home/container
 
-# Installationslogik (bleibt unverändert)
+# Installationslogik...
 if [ ! -f "cod2_lnxded" ]; then
-    echo "COD2-Dateien nicht gefunden. Starte Download..."
+    echo "COD2-Dateien nicht gefunden. Lade herunter..."
     wget -q -O cod2-server.tar.xz "http://linuxgsm.download/CallOfDuty2/cod2-lnxded-1.3-full.tar.xz"
     tar -xf cod2-server.tar.xz
     rm cod2-server.tar.xz
     chmod +x ./cod2_lnxded
     echo "Installation abgeschlossen."
 else
-    echo "Dateien bereits vorhanden."
+    echo "COD2-Dateien bereits vorhanden."
 fi
 
-# Führe den vom Panel übergebenen Befehl direkt aus.
-# Das Panel selbst wird die Variablen ersetzen.
-echo "Führe Befehl vom Panel aus: $@"
-exec "$@"
+# Baue den Startbefehl manuell mit den vom Panel bereitgestellten Umgebungsvariablen.
+# Dies ist die robusteste Methode.
+START_COMMAND="./cod2_lnxded +set dedicated 2 +set net_ip ${SERVER_IP} +set net_port ${SERVER_PORT} +set logfile 1 +exec ${SERVER_CFG}"
+
+echo "Finaler Befehl (manuell gebaut): ${START_COMMAND}"
+
+# Führe den Befehl aus
+exec ${START_COMMAND}
