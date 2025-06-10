c9958d91f5422cc81240431d57a0b3e2902cd758
Geekbee <39613357+GeekbeeGER@users.noreply.github.com>
2025-06-11 00:48:13 +0200

    Update entrypoint.sh

--- cod2/bin/entrypoint.sh
+++ cod2/bin/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 cd /home/container
 
+# --- Installationslogik (ist perfekt und bleibt unverändert) ---
 if [ ! -f "cod2_lnxded" ]; then
     echo "COD2-Dateien nicht gefunden. Starte Download..."
     wget -q -O cod2-server.tar.xz "http://linuxgsm.download/CallOfDuty2/cod2-lnxded-1.3-full.tar.xz"
@@ -13,13 +14,17 @@ else
     echo "Dateien bereits vorhanden."
 fi
 
+# --- Finale Server-Startlogik ---
+# Überprüfe, ob die Variable SERVER_CFG gesetzt ist, sonst nimm einen Standardwert.
 if [ -z "${SERVER_CFG}" ]; then
     CFG_FILE="server.cfg"
 else
     CFG_FILE="${SERVER_CFG}"
 fi
 
-START_COMMAND="./cod2_lnxded +set dedicated 2 +set net_ip ${SERVER_IP} +set net_port ${SERVER_PORT} +set logfile 1 +exec ${CFG_FILE}"
+# Baue den Startbefehl manuell.
+# HIER IST DIE ÄNDERUNG: Wir ersetzen ${SERVER_IP} durch 0.0.0.0
+START_COMMAND="./cod2_lnxded +set dedicated 2 +set net_ip 0.0.0.0 +set net_port ${SERVER_PORT} +set logfile 1 +exec ${CFG_FILE}"
 
 echo "Finaler, manuell gebauter Startbefehl: ${START_COMMAND}"
 exec ${START_COMMAND}
